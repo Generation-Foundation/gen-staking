c7d350cca1d477d67fb4c57e1f2e06353ac6a7a0
Booyoun <babyfungus@naver.com>
2023-03-28 14:54:51 +0900

    Update GateKeeperStaking.sol

--- contracts/GateKeeperStaking.sol
+++ contracts/GateKeeperStaking.sol
@@ -18,9 +18,13 @@ contract Staking is Ownable {
     IERC20 public immutable stakingToken;
     IERC20 public immutable rewardToken;
 
-    // 500 -> 5%, 4000 -> 40%, 10000 -> 100%
-    // 0 ~ 65536
-    uint16 public apr = 4000;
+    // 10000 -> 100%, 498 -> 4.98%, 500 -> 5%, 
+    // 400000 -> 4000%, 390715 -> 3907.15 %
+    // 0 ~ 4294967296
+    uint32 public apr = 498;
+
+    // Fever Staking을 위한 최소 GEN Staking 조건
+    uint256 public minimumGenStaked = 100000000000000000000;
 
     // 전체 게이트키퍼의 스테이킹 총량
     uint256 public totalStaked;
@@ -36,7 +40,12 @@ contract Staking is Ownable {
         stakingFlag = true;
     }
 
-    // 이 컨트랙트가 GEN Staking 인가? Fever Staking 인가?
+    // [주의] Fever Staking의 경우 GEN Staking 컨트랙트 주소를 입력해야 한다.
+    function setGenStakingContractAddress(address contractAddress) onlyOwner public {
+        genStakingContractAddress = contractAddress;
+        emit GenStakingAddressUpdated(genStakingContractAddress);
+    }
+
     function getStakingType() public view returns (string memory) {
         string memory result ;
         if (stakingToken == rewardToken) {
@@ -51,17 +60,11 @@ contract Staking is Ownable {
         return staked[userAddress];
     }
 
-    // Fever Staking의 경우 GEN Staking 컨트랙트 주소를 입력해야 한다.
-    function setGenStakingContractAddress(address contractAddress) onlyOwner public {
-        genStakingContractAddress = contractAddress;
-        emit GenStakingAddressUpdated(genStakingContractAddress);
-    }
-
     // 이 컨트랙트가 Fever Staking 컨트랙트라면 GEN Staking을 100 GEN 이상 하고 있는지 체크하기
     function isValidGateKeeper(address userAddress) internal view returns (bool) {
         uint256 stakedAmount = GenStakingInterface(genStakingContractAddress).getStakedAmount(userAddress);
         bool result = false;
-        if (stakedAmount >= 100000000000000000000) {
+        if (stakedAmount >= minimumGenStaked) {
             result = true;
         }
         return result;
@@ -115,7 +118,6 @@ contract Staking is Ownable {
     }
 
     function estimateReward() onlyGateKeeper public view returns (uint256) {
-        // require(staked[msg.sender] > 0, "staked is <= 0");
         uint256 secondsStaked = block.timestamp - stakedFromTS[msg.sender];
         uint256 rewardsYear = staked[msg.sender] * secondsStaked / 3.154e7;
         uint256 tempReward = SafeMath.mul(rewardsYear, apr);
@@ -123,12 +125,19 @@ contract Staking is Ownable {
         return rewards;
     }
 
-    function setApr(uint16 _apr) onlyOwner public {
-        require(_apr >= 0 && _apr <= 65536, "Invalid APR(_apr > 0 && _apr <= 65536, 10000 == 100%)");
-        apr = _apr;
+    function setMinimumGenStaked(uint256 _minimumGenStaked) onlyOwner public {
+        require(_minimumGenStaked >= 0, "Invalid minimumGenStaked(minimumGenStaked >= 0)");
+        minimumGenStaked = _minimumGenStaked;
+        emit AprUpdated(apr);
     }
 
-    function setStakingFlag (bool _stakingFlag) onlyOwner public {
+    function setApr(uint32 _apr) onlyOwner public {
+        require(_apr >= 0 && _apr <= 4294967296, "Invalid APR(_apr > 0 && _apr <= 4294967296, 10000 == 100%)");
+        apr = _apr;
+        emit AprUpdated(apr);
+    }
+
+    function setStakingFlag(bool _stakingFlag) onlyOwner public {
         stakingFlag = _stakingFlag;
         emit StakingFlagUpdated(stakingFlag);
     }
@@ -153,4 +162,5 @@ contract Staking is Ownable {
     event Unstake(address indexed from, uint256 amount);
     event Claim(address indexed to, uint256 amount);
     event GenStakingAddressUpdated(address contractAddress);
+    event AprUpdated(uint32 apr);
 }
